0ec77eb921267db8079f0ccaf5f06af89337d338
JOCO <111356420+Jocooh@users.noreply.github.com>
2023-04-07 15:40:00 +0900

    feat. 지도 api 수정 / 복기

--- components/home/Map.tsx
+++ components/home/Map.tsx
@@ -13,12 +13,13 @@ type Props = {
 
 const Map = ({
   mapId = 'map',
-  initialCenter = INITIAL_CENTER,
-  initialZoom = INITIAL_ZOOM,
-  onLoad,
+  initialCenter = INITIAL_CENTER, // NITIAL_CENTER = [37.5262411, 126.99289439];
+  initialZoom = INITIAL_ZOOM, //10
+  onLoad, //map이 로드되면 콘솔에 찍힘
 }: Props) => {
   const mapRef = useRef<NaverMap | null>(null);
 
+  //https://navermaps.github.io/maps.js.ncp/docs/naver.maps.html#.MapOptions
   const initializeMap = () => {
     const mapOptions = {
       center: new window.naver.maps.LatLng(...initialCenter),
@@ -30,10 +31,9 @@ const Map = ({
         position: naver.maps.Position.BOTTOM_LEFT,
       },
     };
-
-    const map = new window.naver.maps.Map(mapId, mapOptions);
-    mapRef.current = map;
-
+    //https://navermaps.github.io/maps.js.ncp/docs/naver.maps.Map.html
+    const map = new window.naver.maps.Map(mapId, mapOptions); //map instance , mapId: 지도를 넣을 HTML요소의 id // mapOptions: 지도의 옵션 객체
+    mapRef.current = map; //useRef의 현재 값  = current
     if (onLoad) {
       onLoad(map);
     }
@@ -41,17 +41,18 @@ const Map = ({
 
   useEffect(() => {
     return () => {
-      mapRef.current?.destroy();
+      mapRef.current?.destroy(); //다른 페이지가면 지도 없애기
     };
   }, []);
 
+  //https://nextjs.org/docs/api-reference/next/script#onready
   return (
     <>
       <Script
-        strategy="afterInteractive"
+        strategy="afterInteractive" //로딩의 우선순위 설정
         type="text/javascript"
         src={`https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=${process.env.NEXT_PUBLIC_NCP_CLIENT_ID}`}
-        onReady={initializeMap}
+        onReady={initializeMap} //mount될때마다 지도를 부르기 위해 실행 (OnLoad의 경우 처음 한번만 실행된다.)
       />
       <div id={mapId} style={{ width: '100%', height: '100%' }} />
     </>
